827b56d43adc96ffc46d6f0d90cc7a5c7391eb13
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-07-11 18:34:42 +0300

    feat: add CJCC UI topbar

---
+++ pages/cjcc/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Topbar />
+    </div>
+    <ClientOnly>
+        <TurkeyMap />
+    </ClientOnly>
+</template>
+
+<script setup lang="ts">
+useHead({
+    bodyAttrs: {
+        class: 'bg-[#5B7080] font-serif'
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
