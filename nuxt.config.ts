4d9108739ceb1c007ced0f72e050d2772473d420
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-07-11 18:34:21 +0300

    feat: add Adobe-Garamond-Pro font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxtjs/tailwindcss',
     'nuxt-mongoose',
   ],
   devtools: { enabled: true },
-  mongoose: {
-    uri: process.env.MONGODB_URI,
-    options: {},
-    modelsDir: 'models',
+  nitro: {
+    plugins: ['~/server/index.ts']
+  },
+  app: {
+    head: {
+      link: [
+        { rel:"stylesheet", href: "https://use.typekit.net/ipw2eyg.css" }
+      ]
+    }
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            serif: ['adobe-garamond-pro']
+          }
+        }
+      }
+    }
   }
 })
